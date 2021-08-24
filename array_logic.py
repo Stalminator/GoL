7934f28a6b978904f6278b73e9551c89539209de
stalminator <stalminator@gmail.com>
2021-08-24 15:31:25 +0200

    +manual cell addition +more functions +experimental features manual

--- array_logic.py
+++ array_logic.py
@@ -1,4 +1,6 @@
-import random
+import random, pygame
+import sys
+
 
 def create_array(size):
     cells = []
@@ -9,9 +11,11 @@ def create_array(size):
         cells.append(tmp)
     return cells
 
+
 def fill_random_cells(cells, size):
-    for i in range(size*10):
+    for i in range(size * 10):
         cells[random.randint(0, size - 1)][random.randint(0, size - 1)] = 1
+
 
 def check_single_cell(cells, x, y):
     try:
@@ -19,6 +23,7 @@ def check_single_cell(cells, x, y):
             return True
     except:
         return False
+
 
 def check_neighborhood(cells, i, j):
     s = 0
@@ -32,6 +37,7 @@ def check_neighborhood(cells, i, j):
     if check_single_cell(cells, i + 1, j + 1): s += 1
     return s
 
+
 def array_update(cells, size):
     tmp = []
     for i in range(size):
@@ -42,3 +48,39 @@ def array_update(cells, size):
                 tmp.append([i, j, 0])
     for i in tmp:
         cells[i[0]][i[1]] = i[2]
+
+
+def pygame_draw_rect(win, BLACK, cells, size):
+    for i in range(size):
+        for j in range(size):
+            if cells[i][j] == 1:
+                pygame.draw.rect(win, BLACK, (i * 10 + 2, j * 10 + 2, 8, 8))
+            else:
+                pygame.draw.rect(win, (255,255,255), (i * 10 + 2, j * 10 + 2, 8, 8))
+
+def pygame_draw_lines(win, BLACK, edge):
+    for i in range(0, edge, 10):
+        pygame.draw.line(win, BLACK, (0, i), (edge, i), 2)
+        pygame.draw.line(win, BLACK, (i, 0), (i, edge), 2)
+
+
+def manual_add(cells, win, BLACK, size, edge):
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                x, y = pygame.mouse.get_pos()
+                x = x // 10
+                y = y // 10
+                cells[x][y] = 0 if cells[x][y] == 1 else 1
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_o:
+                    run = False
+
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        pygame_draw_lines(win, BLACK, edge)
+        pygame_draw_rect(win, BLACK, cells, size)
+        pygame.display.update()
--- main.py
+++ main.py
@@ -21,12 +21,22 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+
         #experimental features
+
+        # click - pause and manual add
+        # o - unpause
+        # 1 - speed speed
+        # 2 - low speed
+        # s - clear screen
+        # d - fill with random rectangles
+
         if event.type == pygame.MOUSEBUTTONDOWN:
-            x, y = pygame.mouse.get_pos()
-            x = (x // 10)# * 10
-            y = (y // 10)# * 10
-            cells[x][y] = 1
+            array_logic.manual_add(cells,win,BLACK,size,edge)
+            #x, y = pygame.mouse.get_pos()
+            #x = (x // 10)
+            #y = (y // 10)
+
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_s:
                 cells = array_logic.create_array(size)
@@ -36,16 +46,11 @@ while True:
                 speed = 0
             if event.key == pygame.K_2:
                 speed = 1
-        #'''
-    for i in range(0, edge, 10):
-        pygame.draw.line(win, BLACK, (0, i), (edge, i), 2)
-        pygame.draw.line(win, BLACK, (i, 0), (i, edge), 2)
+        # end of experimental features
 
-    for i in range(size):
-        for j in range(size):
-            if cells[i][j] == 1:
-                pygame.draw.rect(win, BLACK, (i * 10 + 2, j * 10 + 2, 8, 8))
 
+    array_logic.pygame_draw_lines(win,BLACK,edge)
+    array_logic.pygame_draw_rect(win, BLACK, cells, size)
     array_logic.array_update(cells, size)
     pygame.display.update()
     if speed == 1: time.sleep(0.2)
